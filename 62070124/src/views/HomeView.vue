08edae4b8ec31ad935f97390bbd6fde911144adc
maurelle <pk.ap099@gmail.com>
2022-05-10 17:53:51 +0700

    edit: code

---
+++ 62070124/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo_ban.jpg" style="max-width: 100%;">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+
+  <div>
+    <button variant="success">ลงทะเบียนจองคิว - Register</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
